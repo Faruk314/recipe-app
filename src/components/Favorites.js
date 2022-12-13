07abda6c633b13326e6843bc02c91dd484c69f56
Faruk Spahic <farukspahictz@gmail.com>
2022-12-13 17:41:32 +0100

    Add alt to img

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -14,6 +14,7 @@ const Favorites = () => {
               onClick={() => selectMeal(id)}
               className="favorites-img"
               src={image}
+              alt="food"
             />
             <button
               onClick={() => removeFromFavorites(id)}
